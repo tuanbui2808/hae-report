081976ae57fa64b3987b5aebeb8e10614aa1b855
Tuan Bui <tuan.bui2808@gmail.com>
2020-07-14 17:09:16 +0700

    remove brush

--- src/charts/BrushBarChart.js
+++ src/charts/BrushBarChart.js
@@ -22,7 +22,6 @@ export default class Example extends PureComponent {
                     <Tooltip />
                     <Legend verticalAlign="bottom" wrapperStyle={{ lineHeight: '40px' }} />
                     <ReferenceLine y={0} stroke="#000" />
-                    <Brush dataKey="date" height={30} stroke="#8884d8" />
                     <Bar dataKey="Passes" fill="#c1e2c2" />
                     <Bar dataKey="Failures" fill="#ff97a2" />
                     <Bar dataKey="Pending" fill="#8bd5f9" />
